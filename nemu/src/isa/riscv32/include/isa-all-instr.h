56c78a6143b4d71f471f497f327b6815da884ce0
oxygenkun <oxygenkun.1@gmail.com>
2022-03-16 01:07:49 +0800

    [pa2] add shift logic

--- nemu/src/isa/riscv32/include/isa-all-instr.h
+++ nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -6,7 +6,7 @@
     f(jal) f(jalr)\
     f(beq) f(bne) f(blt) f(bltu) f(bge) f(bgeu) \
     f(addi) f(slti) f(sltiu) f(slli)\
-    f(add) f(sub) f(slt) f(sltu) f(xor) f(or) f(and)\
+    f(add) f(sub) f(slt) f(sltu) f(xor) f(or) f(and) f(sll) f(srl) f(sra)\
     f(lw) f(sw) \
     f(inv) f(nemu_trap)
 
--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -26,3 +26,9 @@ def_EHelper(xor) { rtl_xor(s, ddest, dsrc1, dsrc2); }
 def_EHelper(or) { rtl_or(s, ddest, dsrc1, dsrc2); }
 
 def_EHelper(and) { rtl_and(s, ddest, dsrc1, dsrc2); }
+
+def_EHelper(sll) { rtl_sll(s, ddest, dsrc1, dsrc2); }
+
+def_EHelper(srl) { rtl_srl(s, ddest, dsrc1, dsrc2); }
+
+def_EHelper(sra) { rtl_sra(s, ddest, dsrc1, dsrc2); }
