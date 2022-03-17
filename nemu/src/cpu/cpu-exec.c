e937f0826bce4af3fca94c4f266eaaad93ba802e
oxygenkun <oxygenkun.1@gmail.com>
2022-03-17 20:09:44 +0800

    [pa2] iringbuff update

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -147,16 +147,18 @@ void cpu_exec(uint64_t n) {
 
   switch (nemu_state.state) {
     case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
-
-    case NEMU_END: case NEMU_ABORT:
+    case NEMU_ABORT:
+#ifdef CONFIG_ITRACE_COND
+  ringbuff_print();
+#endif  
+    // fall through
+    case NEMU_END: 
       Log("nemu: %s at pc = " FMT_WORD,
           (nemu_state.state == NEMU_ABORT ? ASNI_FMT("ABORT", ASNI_FG_RED) :
            (nemu_state.halt_ret == 0 ? ASNI_FMT("HIT GOOD TRAP", ASNI_FG_GREEN) :
             ASNI_FMT("HIT BAD TRAP", ASNI_FG_RED))),
           nemu_state.halt_pc);
-#ifdef CONFIG_ITRACE_COND
-  ringbuff_print();
-#endif   
+ 
       // fall through
     case NEMU_QUIT: statistic();
   }
