2ac926f970d710f49c7a3887f8b64fdd50072c2f
oxygenkun <oxygenkun.1@gmail.com>
2022-03-21 22:53:42 +0800

    [pa2] kli malloc temp solution

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -29,12 +29,18 @@ int atoi(const char* nptr) {
   return x;
 }
 
+extern char _heap_start;
+void* malloc_p = &_heap_start;
+
 void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
+  // TODO: [pa2]easy implement
+  void* ret = malloc_p;
+  malloc_p = ((uint32_t*)malloc_p + size);
+  return ret;
 #endif
   return NULL;
 }
