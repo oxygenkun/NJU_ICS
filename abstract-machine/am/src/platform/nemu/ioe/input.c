19f8ba8237f0932884de071f3fc85b81e9159391
oxygenkun <oxygenkun.1@gmail.com>
2022-03-22 13:17:49 +0800

    [pa2] add keyboard input

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,6 +4,8 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  // read the keyboard value from nemu MMIO
+  uint32_t input = inl(KBD_ADDR);
+  kbd->keydown = (input & KEYDOWN_MASK ? true : false);
+  kbd->keycode = input & ~KEYDOWN_MASK;
 }
