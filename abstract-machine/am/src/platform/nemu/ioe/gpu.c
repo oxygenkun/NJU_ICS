ae2bba6db0fd37827ca03cae621cd179f09ec9e4
oxygenkun <oxygenkun.1@gmail.com>
2022-03-22 20:19:54 +0800

    [pa2] gpu

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -17,7 +17,7 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
     .width = inw(VGACTL_ADDR+2), .height = inw(VGACTL_ADDR),
-    .vmemsz = width * height * sizeof(uint32_t)
+    .vmemsz = inw(VGACTL_ADDR+2) * inw(VGACTL_ADDR) * sizeof(uint32_t)
   };
 }
 
